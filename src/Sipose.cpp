ff92f9e290dbf410571593d8c2fe691a8fb7e063
aancw <cacaddv@gmail.com>
2014-03-07 17:15:28 +0700

    Sipose Version 2.0 With two Scanning Mode

--- src/Sipose.cpp
+++ src/Sipose.cpp
@@ -1,9 +1,23 @@
 /*
-author : aancw
-Created on : 06 Jan 2014
-OS : Windows 7 x64,Linux debian Wheezy
-Compiler : g++ (Debian 4.7.2-5) 4.7.2
-Version : v1.3
+	
+	Author : aancw
+	Created on : 07 Mar 2014
+	Tested OS : Windows 7 x64,Linux debian Wheezy
+	Compiler : g++ (Debian 4.7.2-5) 4.7.2
+	Version : v2.0
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
@@ -41,18 +55,20 @@ Version : v1.3
 // Include File untuk semua OS
 #include <sstream> /* untuk string stream*/
 #include <fstream> /* untuk fopen,fwrite*/
+#include <ctime>
+
 using namespace std;
 
 void CONSOLE_Print(string message)
 {
-	cout << message<<endl;
+	cout << message <<endl;
 	
 	// Logging cout to a file
 	ofstream myfile;
-	myfile.open ("Logging.txt",ios::app);
+	myfile.open ("Sipose.log",ios::app);
 	if( !myfile.fail( ) )
 	{
-		myfile << message<<endl;
+		myfile << message <<endl;
 		myfile.close();
 	}
 	
@@ -61,7 +77,7 @@ void CONSOLE_Print(string message)
 void DEBUG_Print(string message)
 {
 	// It's just cout 
-	cout<<message<<endl;
+	cout << message <<endl;
 }
 
 string UTIL_ToString( unsigned int i )
@@ -73,57 +89,89 @@ string UTIL_ToString( unsigned int i )
 	SS >> result;
 	return result;
 }
-int main (){
-	//---------------------
-	// Deklarasi Variabel author,version,appName
-	string author = "aancw";
-	string appName = "Sipose";
-	string version = "v1.3";
-
-	//---------------------
-	// Variable untuk Ip Address dan port yang akan dikoneksikan ke server
-	struct hostent *alamat2;
-	int Start,End;
-	char alamat1[20];
-	u_long iMode=0;
+string dateTime()
+{
+	//----------------------------
+	// Untuk check waktu expired dan delete admin berkala by aan
 	
 	//-------------------
-	// Inisialisasi Winsock
+	//current date/time based on current system
+	time_t now	= time(0);
+	tm *ltm		= localtime(&now);
+	
+	//-------------------
+   // Pisahkan satu persatu date dan time untuk mempermudah manipulasi data
+   // Localtime sudah dirubah menjadi WIB atau GMT + 7 
+   int Hari		= ltm->tm_mday;
+   int Bulan	= 1 + ltm->tm_mon;
+   int Tahun	= 1900 + ltm->tm_year;
+   int Jam		= ltm->tm_hour;
+   int Menit	= ltm->tm_min - 8;
+   int Detik	= ltm->tm_sec;
+   
+	//---------------
+	// You Need to convert from Const Char* To String :D
+	string HariStr	= UTIL_ToString(Hari);
+	string BulanStr = UTIL_ToString(Bulan);
+	string TahunStr	= UTIL_ToString(Tahun);
+	string JamStr	= UTIL_ToString(Jam);
+	string MenitStr = UTIL_ToString(Menit);
+	string DetikStr	= UTIL_ToString(Detik);
+
+	string Waktu = "["+HariStr+"/"+BulanStr+"/"+TahunStr+" "+JamStr+":"+MenitStr+":"+DetikStr+"]  ";
+	return Waktu;
+}
+int scanPortWithSingle(int iResult,struct hostent *alamat2,int SinglePort)
+{
+	//--------------------
+	// Membuat Socket untuk koneksi ke server
+	int Socket;
+	Socket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 	#ifdef _WIN32
-		WSADATA wsaData;
-		int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
-		if (iResult != NO_ERROR)
-		{
-			CONSOLE_Print( "WSAStartup failed with error : "+UTIL_ToString(iResult));
-			return 1;
-		}
+			if(Socket == INVALID_SOCKET)
+			{
+				CONSOLE_Print("Socket function failed with error : "+WSAGetLastError());
+				WSACleanup();
+				return 1;
+			}
 	#endif
 
-	CONSOLE_Print(appName+" "+version+" by "+author);
-	DEBUG_Print("Input Valid Host Address(ex: localhost or 127.0.0.1) : ");
-	cin>>alamat1;
-	DEBUG_Print("Enter Port Range...");
-	DEBUG_Print("Start : ");
-	cin>>Start;
-	DEBUG_Print("End : ");
-	cin>>End;
-#ifdef _WIN32
-	system("CLS");//clear the screen
-#elif __linux__
-	printf("\033[H\033[J");
-#endif
-	CONSOLE_Print("Begin Port Scanning : "+string(alamat1)+"...\n");
-	alamat2 = gethostbyname(alamat1);
+	//-------------------
+	// Untuk Sockaddr seperti ip address dan port yang akan terkoneksi
+	sockaddr_in SockAddr;
+	SockAddr.sin_family=AF_INET;
+	SockAddr.sin_addr.s_addr = *((unsigned long*)alamat2->h_addr); 
+	SockAddr.sin_port = htons(SinglePort);
 
-	CONSOLE_Print("Port\tStatus");
+	if (connect(Socket, (sockaddr*)(&SockAddr), sizeof(SockAddr)) == 0)
+	{
+		//CONSOLE_Print(UTIL_ToString(Start)+"\tclose :");
+		CONSOLE_Print(dateTime() + "[PROCCESS] " + UTIL_ToString(SinglePort)+"\tOpen");
+	}
+	else
+	CONSOLE_Print(dateTime() + "[PROCCESS] " + UTIL_ToString(SinglePort)+"\tClosed");
 
+	#ifdef _WIN32
+		iResult = closesocket(Socket);
+		if(iResult == SOCKET_ERROR)
+		{
+			CONSOLE_Print("closesocket function failed with error"+WSAGetLastError());
+			WSACleanup();
+			return 1;
+		}
+	#elif __linux__
+		close(Socket);
+	#endif
+}
+int scanPortWithRange(int Start,int End,int iResult,struct hostent	*alamat2)
+{
 	for (Start; Start<=End; Start++)
 	{
 	
 		//--------------------
 		// Membuat Socket untuk koneksi ke server
 		int Socket;
-		Socket=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+		Socket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 		#ifdef _WIN32
 				if(Socket == INVALID_SOCKET)
 				{
@@ -143,7 +191,7 @@ int main (){
 		if (connect(Socket, (sockaddr*)(&SockAddr), sizeof(SockAddr)) == 0)
 		{
 			//CONSOLE_Print(UTIL_ToString(Start)+"\tclose :");
-			CONSOLE_Print(UTIL_ToString(Start)+"\tOpen");
+			CONSOLE_Print(dateTime() + "[PROCCESS] " + UTIL_ToString(Start)+"\tOpen");
 		}
 
 		#ifdef _WIN32
@@ -158,8 +206,89 @@ int main (){
 			close(Socket);
 		#endif
 	}
-	CONSOLE_Print("End Port Scanning...\n");
+}
+int main (){
+	//---------------------
+	// Deklarasi Variabel author,version,appName
+	string author	= "aancw";
+	string appName	= "Sipose";
+	string version	= "v2.0";
 
+	//---------------------
+	// Variable untuk Ip Address dan port yang akan dikoneksikan ke server
+	struct hostent	*alamat2;
+	int	Start,End,Pilihan,SinglePort;
+	char alamat1[20];
+	u_long iMode=0;
 	
+	//-------------------
+	// Inisialisasi Winsock
+	#ifdef _WIN32
+		WSADATA wsaData;
+		int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+		if (iResult != NO_ERROR)
+		{
+			CONSOLE_Print( dateTime() + "[ERROR] " + "WSAStartup failed with error : "+UTIL_ToString(iResult));
+			return 1;
+		}
+	#endif
+	
+	CONSOLE_Print(dateTime() + "[INFO] " + "Starting Sipose Application");
+	CONSOLE_Print(dateTime() + "[INFO] " +  appName+" "+version+" by "+author);
+	DEBUG_Print("\nPort Scanning Mode ");
+	DEBUG_Print("1. Single Port Scanning Mode");
+	DEBUG_Print("2. Range Port Scanning Mode");
+	DEBUG_Print("\nPlease Select what you want : ");
+	cin>>Pilihan;
+
+	if( Pilihan == 1 )
+	{
+		DEBUG_Print("Enter Valid Host Address(ex: localhost or 127.0.0.1) : ");
+		cin>>alamat1;
+		DEBUG_Print("Enter the ports you want to scan : ");
+		cin>>SinglePort;
+	}
+	else if (Pilihan == 2)
+	{
+		
+		DEBUG_Print("Input Valid Host Address(ex: localhost or 127.0.0.1) : ");
+		cin>>alamat1;
+		DEBUG_Print("Enter Port Range ");
+		DEBUG_Print("Start : ");
+		cin>>Start;
+		DEBUG_Print("End : ");
+		cin>>End;
+		CONSOLE_Print(dateTime() + "[INFO] Scanning speed depends on the speed of the internet and the amount of range that you enter!");
+	}
+	else
+		CONSOLE_Print("Untuk saat ini hanya ada dua pilihan Mode Scanning ");
+
+#ifdef _WIN32
+	system("CLS");//clear the screen
+#elif __linux__
+	printf("\033[H\033[J"); // Linux Clear Screen
+#endif
+
+	alamat2 = gethostbyname(alamat1);
+
+	CONSOLE_Print(dateTime() + "[INFO] Please be patient if you use Range Port Scanning Mode ");
+	CONSOLE_Print(dateTime() + "[PROCCESS] " + "Begin Port Scanning on "+string(alamat1)+"\n");
+	CONSOLE_Print(dateTime() + "[PROCCESS] "  + "Port\tStatus");
+	
+	if( Pilihan == 1)
+	{
+		scanPortWithSingle(iResult,alamat2,SinglePort);
+	}
+	else if( Pilihan == 2)
+	{
+		// Scanning Proccess for Range Scanning Mode
+		scanPortWithRange(Start,End,iResult,alamat2);
+	}
+
+	CONSOLE_Print(dateTime() + "[INFO] " + "End Port Scanning on "+string(alamat1)+"\n");
+
+#ifdef _WIN32
+	system("pause");
+#endif
 	return 0;
 }
